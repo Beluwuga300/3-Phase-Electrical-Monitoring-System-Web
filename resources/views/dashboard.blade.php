446c0791552875ca0c9b37890e59980e3cdd39d6
Cyrillus Gema Satria Tama <gemasatria65@gmail.com>
2025-07-13 03:58:25 +0700

    Enhance dashboard styling by adding google fonts (poppins)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,6 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pemantauan Energi Listrik</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
+    </style>
 </head>
 <body>
     <h1>Data Pemantauan Energi Listrik 3 Fasa</h1>
